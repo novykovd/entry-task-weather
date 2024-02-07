45418a5c19d0ba7e91fb62336f9fb5804cad17be
d <d@webdev.myguest.virtualbox.org>
2024-02-07 03:21:18 +0100

    bonus freatures

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import HeatIndexCalc from './CalcTab';
 
 function App() {
   const [info, setInfo] = useState(null);
+  const [rangeF, setRangeF] = useState(3)
+  const [rangeP, setRangeP] = useState(3)
 
   useEffect(() => {
 
-    const URL = "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&hourly=temperature_2m,relative_humidity_2m,weather_code,surface_pressure&past_days=3&forecast_days=3";
+    const URL = `https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&hourly=temperature_2m,relative_humidity_2m,weather_code,surface_pressure&past_days=${rangeP}&forecast_days=${rangeF}`;
 
 
     fetch(URL).then((response) => response.json()).then((response) => {
@@ -17,13 +19,24 @@ function App() {
       console.log(hourly)
       setInfo(hourly)
     }).catch((error) => {console.log(error)})
-  }, []);
+  }, [rangeF, rangeP]);
+
+  const handleChangeSlider = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    if(event.target.id == 'futureSlider'){
+      setRangeF(newValue)
+    } else {
+      setRangeP(newValue)
+    }
+  }
 
   return (
     <>
       <div className='main'>
         <div className='tab1'>
           <h2>info table</h2>
+          <div>future days forecast<input type="range" min={1} max={3} step={1} value={rangeF} id='futureSlider' onChange={handleChangeSlider}/></div>
+          <div>past days forecast<input type="range" min={0} max={3} step={1} value={rangeP} id='pastSlider' onChange={handleChangeSlider}/></div>
           <MeteoTable info={info}/>
         </div>
         <div className='tab2'>
--- src/CalcTab.jsx
+++ src/CalcTab.jsx
@@ -4,14 +4,15 @@ import './CalcTab.css';
 
 function HeatIndexCalc(){
     const [heatIndex, setIndex] = useState(0);
+    const [historyQueue, setQueue] = useState([]);
+    const [checked, setChecked] = useState(false);
 
 
     const onClickCalc = () => {
       var T = document.getElementById('tempInput').value;
       var rh = document.getElementById('humidityInput').value;
-      var unitC = document.getElementById('c').value;
   
-      if (unitC == 'on') {
+      if (checked === true) {
         T = (9/5)*T + 32
       }
   
@@ -23,29 +24,53 @@ function HeatIndexCalc(){
         - (5.481717*10**-2*rh**2) + (1.22874*10**-3*T**2*rh)
         + (8.5282*10**-4*T*rh**2) - (1.99*10**-6*T**2*rh**2)
   
-        if(unitC == 'on'){
-          HI = (5/9)*(HI-32)
-          setIndex(HI)
-        }else{
-          setIndex(HI)
+        if(checked == true){
+          HI = (HI - 32)*(5/9)
         }
-        
+        HI = HI.toFixed(1);
+        setIndex(HI)        
       }
       
     }
 
+    const checkBoxState = () => {
+      setChecked(!checked)
+    }
+
+    useEffect(() => {
+      if(heatIndex == 0){
+        return
+      }
+      if(historyQueue.length < 5){
+        setQueue((oldQ) => { return [...oldQ, heatIndex]})
+      } else {
+        const [oldItem, ...tempQ] = historyQueue;
+        setQueue(() => { return [...tempQ, heatIndex]})
+      }
+    }, [heatIndex])
+
     return(
-        <div>
+      <div>
+          <div>
             <h2>heat index calc</h2>
             <input type="text" placeholder='enter temperature' id='tempInput'/>
             <input type="text" placeholder='enter humidity'id='humidityInput'/>
-            <div>celsius?<input type="checkbox" id='c'/></div>
+            <div>celsius?<input type="checkbox" id='c' onChange={checkBoxState} checked={checked}/></div>
             <div><button onClick={onClickCalc}>calculate</button></div>
             <div>
-            <p>Heat Index</p>
-            {heatIndex}
+              <p>Heat Index</p>
+              {heatIndex}
             </div>
-        </div>
+          </div>
+          <div>
+            History:
+            <ul>
+              {historyQueue.map((item) => {
+                return(<li>{item}</li>)
+              })}
+            </ul>
+          </div>
+      </div>
     )
 }
 
